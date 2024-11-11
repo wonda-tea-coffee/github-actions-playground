ed0f63f688c530cb2094b0201d9a02c825be0e3a
Rikita Ishikawa <lagrange.resolvent@gmail.com>
2024-11-11 12:21:03 +0900

    yoshi

---
+++ test.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+GITHUB_REPOSITORY=wonda-tea-coffee/github-actions-playground
+LAST_WEEK=$(date --date='7 days ago' +%Y-%m-%d)
+SEARCH_QUERY="repo:$GITHUB_REPOSITORY is:open created:>=$LAST_WEEK"
+
+gh api graphql -f query="
+  {
+    search(query: \"$SEARCH_QUERY\", type: DISCUSSION, first: 100) {
+      edges {
+        node {
+          ... on Discussion {
+            url
+          }
+        }
+      }
+    }
+  }" | \
+  jq -r '.data.search.edges[].node.url'
